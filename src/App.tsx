14a82e75248ac7eb1d2079512187a0a621aac870
Vishant Kalwani <vishanto95@gmail.com>
2023-09-19 20:11:50 +0530

    Setup initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import { Main } from './pages/main';
+import { Login } from './pages/login';
+import { Navbar } from './components/navbar';
+import { CreatePost } from './pages/create-post/create-post';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/createpost" element={<CreatePost />} />
+          <Route path="*" element={<h1>Page not Found!</h1>} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
