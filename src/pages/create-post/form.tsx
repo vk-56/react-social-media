8dc86dff1c7c6eb523f7cbc637ed6a4fad408375
Vishant Kalwani <vishanto95@gmail.com>
2023-09-19 20:28:09 +0530

    Created simple form for posts

--- src/pages/create-post/form.tsx
+++ src/pages/create-post/form.tsx
@@ -1,7 +1,36 @@
-export const CreateForm = () => {
-    return (
-        <form>
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import '../../styles/form.css'
 
+interface CreateFormData {
+    title: string;
+    description: string;
+}
+
+
+export const CreateForm = () => {
+    const schema = yup.object().shape({
+        title: yup.string().required("You must add a title"),
+        description: yup.string().max(200).required("You must add a description"),
+
+    })
+
+    const { register, handleSubmit, formState: {errors} } = useForm<CreateFormData>({
+        resolver: yupResolver(schema),
+    })
+
+    const onCreatePost = (data : CreateFormData) => {
+        console.log(data)
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onCreatePost)}>
+            <input placeholder='Title...' {...register("title")}/>
+            <p> {errors.title?.message} </p>
+            <textarea placeholder='Description...' {...register("description")}/>
+            <p> {errors.description?.message} </p>
+            <input type='submit' />
         </form>
     );
 };
